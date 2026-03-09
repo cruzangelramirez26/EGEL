fa2536fc1c0f8615667d23e4ec9c9835bad86cb3
cruzangelramirez26 <cruzangelramirez26@gmail.com>
2026-03-08 21:23:20 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import StudyPlan from "./egel_study_plan_v2";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<StudyPlan />);
