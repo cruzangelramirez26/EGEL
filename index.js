6e34484777790693d683364a89bd00b37e82449e
cruzangelramirez26 <cruzangelramirez26@gmail.com>
2026-03-08 21:36:52 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import StudyPlan from "./egel_study_plan_v2";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<StudyPlan />);
